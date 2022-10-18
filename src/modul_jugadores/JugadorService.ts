bc1e91c9522722630fd69251aa4506e58652cc56
Luke_skyguy <mikaur00@gmail.com>
2022-10-18 15:17:59 +0200

    try_tasks

--- src/modul_jugadores/JugadorService.ts
+++ src/modul_jugadores/JugadorService.ts
@@ -9,85 +9,85 @@ import { Tag } from "src/modul_tags/tag.entity";
 
 @Injectable()
 export class JugadorService {
-    
-    constructor(
-        @InjectRepository(jugadores) private jugadorRepository: Repository<jugadores>,
-        @InjectRepository(Tag) private tagRepository: Repository<Tag>) { }
-        
-    // example how to show DM entity
-    showAllRepository() {
-        return this.jugadorRepository.find();
-        //const player = { name: 'Villa', team: 'Atletico', num: 7 };
-        //await playerRepository.save(player);
-      
-    }
-     // Busqueda con relacion a una entidad
-    searchRepository(id: number) {
 
-        return this.jugadorRepository.find({ idJugador: id });
-    }
-    // Busqueda con relacion a dos entidades
-    async searchPlayerTeam(id: number) {
-       
-        return this.jugadorRepository.find({
-            where: { idJugador: id },
-            relations: ['equipo']
-        });
-    }
-    // Busqueda paginada
-     searchAllPaged(limit:number,offset:number) {
-        return this.jugadorRepository.find( {
-            skip:offset,
-            take: limit,
-        });
-    }
-    async insertPlayer(newPlayer: jugadores): Promise<jugadores | undefined> {
-        return await this.jugadorRepository.save(newPlayer);
+  constructor(
+    @InjectRepository(jugadores) private jugadorRepository: Repository<jugadores>,
+    @InjectRepository(Tag) private tagRepository: Repository<Tag>) { }
 
-    }
-    async removePlayer(id: number) {
-        const exist = await this.jugadorRepository.findOne({ idJugador: id })
-        if (!exist) throw new NotFoundException('Este post no existe');
+  // example how to show DM entity
+  showAllRepository() {
+    return this.jugadorRepository.find();
+    //const player = { name: 'Villa', team: 'Atletico', num: 7 };
+    //await playerRepository.save(player);
 
-        return await this.jugadorRepository.delete(id);
-    }
-    async updatePlayer(id: number, newPlayer: jugadores) {
-        let updatedPlayer: jugadores = await this.jugadorRepository.findOne({ idJugador: id });
-        updatedPlayer = { ...updatedPlayer, ...newPlayer }
-        return await this.jugadorRepository.save(updatedPlayer);
+  }
+  // Busqueda con relacion a una entidad
+  searchRepository(id: number) {
 
-    }
+    return this.jugadorRepository.find({ idJugador: id });
+  }
+  // Busqueda con relacion a dos entidades
+  async searchPlayerTeam(id: number) {
 
-    //Cambio de tag de player
-    async assignPlayerToTag(
-        idPlayer:number,
-        tagId?: number,
-      ): Promise<jugadores> {
-        if (idPlayer) {
-          const player = (await this.jugadorRepository.findOne({
-            where: {
-              idJugador: idPlayer,
-              idTag: tagId,
-            },
-            //  relations: ['tags'],
-          })) as jugadores;
-    
-          if (player) {
-            const tag = (await this.tagRepository.findOne({
-              where: {
-                idJugador: idPlayer,
-                idTag: tagId,
-              },
-            })) as Tag;
-    
-            if (tag) {
-              player.tag.push(tag);
-              this.jugadorRepository.save(player);
-              return player;
-            }
-          }
+    return this.jugadorRepository.find({
+      where: { idJugador: id },
+      relations: ['equipo']
+    });
+  }
+  // Busqueda paginada
+  searchAllPaged(limit: number, offset: number) {
+    return this.jugadorRepository.find({
+      skip: offset,
+      take: limit,
+    });
+  }
+  async insertPlayer(newPlayer: jugadores): Promise<jugadores | undefined> {
+    return await this.jugadorRepository.save(newPlayer);
+
+  }
+  async removePlayer(id: number) {
+    const exist = await this.jugadorRepository.findOne({ idJugador: id })
+    if (!exist) throw new NotFoundException('Este post no existe');
+
+    return await this.jugadorRepository.delete(id);
+  }
+  async updatePlayer(id: number, newPlayer: jugadores) {
+    let updatedPlayer: jugadores = await this.jugadorRepository.findOne({ idJugador: id });
+    updatedPlayer = { ...updatedPlayer, ...newPlayer }
+    return await this.jugadorRepository.save(updatedPlayer);
+
+  }
+
+  //Cambio de tag de player
+  async assignPlayerToTag(
+    idPlayer: number,
+    tagId?: number,
+  ): Promise<jugadores> {
+    if (idPlayer) {
+      const player = (await this.jugadorRepository.findOne({
+        where: {
+          idJugador: idPlayer,
+          idTag: tagId,
+        },
+        relations: ['tags'],
+      })) as jugadores;
+
+      if (player) {
+        const tag = (await this.tagRepository.findOne({
+          where: {
+            idJugador: idPlayer,
+            idTag: tagId,
+          },
+        })) as Tag;
+
+        if (tag) {
+          player.push(tag);
+          this.jugadorRepository.save(player);
+          return player;
         }
-        return null;
       }
+    }
+    return null;
+  }
 }
 
